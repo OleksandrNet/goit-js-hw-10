20629b257ce8274fa5755fddc55363690cff826f
Oleksandr <netesa.sasha@gmail.com>
2024-07-01 18:50:39 +0300

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -44,7 +44,7 @@ startBtn.addEventListener('click', () => {
   const countdownInterval = setInterval(() => {
     const currentTime = new Date();
     const timeDifference = userSelectedDate - currentTime;
-    if (timeDifference <= 0) {
+    if (timeDifference <= 1000) {
       clearInterval(countdownInterval);
       dateTimePicker.disabled = false;
       iziToast.success({ message: 'Countdown completed!' });
